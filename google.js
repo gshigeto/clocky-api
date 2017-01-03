73abc982f31dad1c7bf853c8fbdcbd0f3257389a
Gavin Nitta <gshigeto@gmail.com>
2017-01-02 19:39:46 -0700

    Add google sheets authorization

---
+++ google.js
@@ -0,0 +1,87 @@
+var fs = require('fs');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+var Q = require('q');
+
+function clockIn(token, docId) {
+  authorize(token).then(function (client) {
+    listMajors(client, docId);
+  });
+}
+
+function clockOut(token) {
+  authorize(token).then(function (client) {
+    listMajors(client, docId);
+  });
+}
+
+/**
+ * Authorize with the given token
+ *
+ * @param {Object} token The authorization client tokens.
+ */
+function authorize(token) {
+  var promise = Q.defer();
+  // Load client secrets from a local file.
+  fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+    if (err) {
+      promise.reject('Error loading client secret file: ' + err)
+    }
+    // Authorize a client with the loaded credentials, then call the
+    // Google Sheets API.
+    promise.resolve(createOauthClient(JSON.parse(content), token));
+  });
+  return promise.promise;
+}
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {Object} token The authorization client tokens.
+ */
+function createOauthClient(credentials, token, callback, docId) {
+  var clientSecret = credentials.installed.client_secret;
+  var clientId = credentials.installed.client_id;
+  var redirectUrl = credentials.installed.redirect_uris[0];
+
+  var auth = new googleAuth();
+  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+  oauth2Client.credentials = token;
+  return oauth2Client;
+}
+
+/**
+ * Print the names and majors of students in a sample spreadsheet:
+ * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ */
+function listMajors(auth, docId) {
+  var sheets = google.sheets('v4');
+  sheets.spreadsheets.values.get({
+    auth: auth,
+    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+    range: 'Class Data!A2:E',
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    var rows = response.values;
+    if (rows.length == 0) {
+      console.log('No data found.');
+    } else {
+      console.log('Name, Major:');
+      for (var i = 0; i < rows.length; i++) {
+        var row = rows[i];
+        // Print columns A and E, which correspond to indices 0 and 4.
+        console.log('%s, %s', row[0], row[4]);
+      }
+    }
+  });
+}
+
+module.exports = {
+  clockIn: clockIn,
+  clockOut: clockOut
+};
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 var cors = require('cors');
 const app = express();
+var google = require('./google.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,14 +12,24 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.post('/in', (req, res) => {
+app.post('/in/:docId', (req, res) => {
+  google.clockIn(createToken(req.body), req.params.docId);
   res.send({message: 'CLOCKED IN'});
 });
 
-app.post('/out', (req, res) => {
+app.post('/out/:docId', (req, res) => {
+  google.clockOut(createToken(req.body), req.params.docId);
   res.send({message: 'CLOCKED OUT'});
 });
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!');
 });
+
+function createToken(body) {
+  return {
+    access_token: body.access_token,
+    refresh_token: body.refresh_token,
+    token_type: body.token_type
+  };
+}
